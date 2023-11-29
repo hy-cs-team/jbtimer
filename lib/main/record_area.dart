a637e347e62db38622d9c5d6285d366df9f89c0e
seungjoonoh <jason.noh@nectarsodivine.com>
2023-11-30 00:20:43 +0900

    feat: turn record_area to a stateful widget

--- lib/main/record_area.dart
+++ lib/main/record_area.dart
@@ -5,15 +5,22 @@ import 'package:jbtimer/data/record.dart';
 import 'package:jbtimer/jb_component.dart';
 import 'package:jbtimer/main/session_controller.dart';
 
-class RecordArea extends StatelessWidget {
+class RecordArea extends StatefulWidget {
   final SessionController sessionController;
   const RecordArea({super.key, required this.sessionController});
 
   @override
+  State<RecordArea> createState() => _RecordAreaState();
+}
+
+class _RecordAreaState extends State<RecordArea> {
+  @override
   Widget build(BuildContext context) {
     return JBComponent(
       onPressed: () {
-        sessionController.add(Record(dateTime: DateTime.now(), recordMs: Random().nextInt(15000) + 15000));
+        widget.sessionController.add(Record(
+            dateTime: DateTime.now(),
+            recordMs: Random().nextInt(15000) + 15000));
       },
       child: const Center(
         child: Text('0:00.000'),
