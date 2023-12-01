417864840c5fdca20588890c70b242ee53247934
seungjoonoh <jason.noh@nectarsodivine.com>
2023-12-01 16:12:44 +0900

    fix: use Stopwatch class to implement timer

--- lib/main/record_area.dart
+++ lib/main/record_area.dart
@@ -27,6 +27,8 @@ enum _RecordState {
   }
 }
 
+const _previewTimeMilli = 15000;
+
 class RecordArea extends StatefulWidget {
   final SessionController sessionController;
   const RecordArea({super.key, required this.sessionController});
@@ -37,81 +39,80 @@ class RecordArea extends StatefulWidget {
 
 class _RecordAreaState extends State<RecordArea> {
   _RecordState _recordState = _RecordState.idle;
-  int _previewTime = 15;
-  int _elapsedPreviewTime = 0;
-  int _elapsedTime = 0;
-  Timer? _previewCountDownTimer;
-  Timer? _penaltyTimer;
-  Timer? _recordTimer;
   DateTime _startAt = DateTime.now();
+  final Stopwatch _previewStopwatch = Stopwatch();
+  final Stopwatch _stopwatch = Stopwatch();
+  late Timer _timer;
+  final _stopwatchStreamController = StreamController<String>();
+
+  String _formatElapsedTime(Duration d) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    String threeDigits(int n) => n.toString().padLeft(3, '0');
+    final twoDigitMinutes = twoDigits(d.inMinutes.remainder(60));
+    final twoDigitSeconds = twoDigits(d.inSeconds.remainder(60));
+    final threeDigitMilliseconds =
+        threeDigits(d.inMilliseconds.remainder(1000));
+    return "$twoDigitMinutes:$twoDigitSeconds.$threeDigitMilliseconds";
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _stopwatchStreamController.close();
+    super.dispose();
+  }
 
   void startPreview() {
     setState(() {
       _recordState = _RecordState.preview;
     });
-
-    _previewCountDownTimer =
-        Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        _previewTime--;
-        if (_previewTime == 0) {
-          startPenalty();
-          timer.cancel();
-        }
-      });
-    });
-  }
-
-  void startPenalty() {
-    setState(() {
-      _recordState = _RecordState.penalty;
-    });
-
-    _penaltyTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        _elapsedPreviewTime++;
-      });
+    _previewStopwatch.start();
+    _timer = Timer.periodic(const Duration(milliseconds: 10), (_) {
+      final leftPreviewTime = Duration(
+        milliseconds:
+            _previewStopwatch.elapsed.inMilliseconds < _previewTimeMilli
+                ? _previewTimeMilli - _previewStopwatch.elapsed.inMilliseconds
+                : _previewStopwatch.elapsed.inMilliseconds - _previewTimeMilli,
+      );
+      final leftPreviewTimeString = _formatElapsedTime(leftPreviewTime);
+      _stopwatchStreamController.sink.add(leftPreviewTimeString);
     });
   }
 
   void startRecord() {
+    _previewStopwatch.stop();
+    _timer.cancel();
     setState(() {
       _recordState = _RecordState.running;
       _startAt = DateTime.now();
     });
-    _recordTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        _elapsedTime++;
-      });
+
+    _stopwatch.start();
+    _timer = Timer.periodic(const Duration(milliseconds: 10), (_) {
+      final runningTimeString = _formatElapsedTime(_stopwatch.elapsed);
+      _stopwatchStreamController.sink.add(runningTimeString);
     });
-    _penaltyTimer?.cancel();
   }
 
   void stopRecord() {
-    _recordTimer?.cancel();
+    _stopwatch.stop();
+    _timer.cancel();
+    final previewTime = _previewStopwatch.elapsed.inMilliseconds < 15000
+        ? 0
+        : _previewStopwatch.elapsed.inMilliseconds - 15000;
     widget.sessionController.add(Record(
       dateTime: _startAt,
-      recordMs: _elapsedPreviewTime + _elapsedTime,
+      recordMs: previewTime + _stopwatch.elapsed.inMilliseconds,
     ));
     reset();
   }
 
   void reset() {
+    _previewStopwatch.reset();
+    _stopwatch.reset();
     setState(() {
       _recordState = _RecordState.idle;
-      _previewTime = 15;
-      _elapsedPreviewTime = 0;
-      _elapsedTime = 0;
     });
-    _previewCountDownTimer?.cancel();
-    _penaltyTimer?.cancel();
-  }
-
-  @override
-  void dispose() {
-    _previewCountDownTimer?.cancel();
-    _penaltyTimer?.cancel();
-    super.dispose();
   }
 
   void onRecordAreaTapped() {
@@ -134,25 +135,25 @@ class _RecordAreaState extends State<RecordArea> {
 
   @override
   Widget build(BuildContext context) {
-    final displayWidget = _recordState == _RecordState.preview
-        ? Text(
-            'Preview: $_previewTime seconds',
-            style: const TextStyle(fontSize: 24),
-          )
-        : _recordState == _RecordState.penalty
-            ? Text(
-                'Penalty Time: $_elapsedPreviewTime seconds',
-                style: const TextStyle(fontSize: 24),
-              )
-            : Text(
-                'Elapsed Time: $_elapsedTime seconds',
-                style: const TextStyle(fontSize: 24),
-              );
     return JBComponent(
       downColor: _recordState.color,
       onPressed: onRecordAreaTapped,
-      child: Center(
-        child: displayWidget,
+      child: StreamBuilder<String>(
+        stream: _stopwatchStreamController.stream,
+        builder: (context, snapshot) {
+          final isOverPreviewTime = _recordState == _RecordState.preview &&
+              _previewStopwatch.elapsed.inMilliseconds > _previewTimeMilli;
+          final timerTextStyle = TextStyle(
+            fontSize: 24,
+            color: isOverPreviewTime ? Colors.red : Colors.white,
+          );
+          return Center(
+            child: Text(
+              snapshot.data ?? '00:15.000',
+              style: timerTextStyle,
+            ),
+          );
+        },
       ),
     );
   }
